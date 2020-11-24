478dda654479b6d52d04303ebf346b05174e9dc9
Mohamed Amadou <am.awal@outlook.com>
2020-11-24 23:47:42 +0000

    Working code; after removing redundant packages

--- employee/src/main/java/com/tcs/employee/service/EmployeeServiceImpl.java
+++ employee/src/main/java/com/tcs/employee/service/EmployeeServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.tcs.department.model.Department;
 import com.tcs.employee.repository.EmployeeRepository;
 import com.tcs.employee.model.Employee;
 
